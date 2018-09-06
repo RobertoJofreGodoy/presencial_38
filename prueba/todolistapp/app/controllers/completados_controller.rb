fec8d4eff2a300f00e73da68bfd13d6f003b0930
RobertoJofreGodoy <roberto.jofre.godoy@gmail.com>
2018-09-06 20:14:00 -0300

    add photos

--- prueba/todolistapp/app/controllers/completados_controller.rb
+++ prueba/todolistapp/app/controllers/completados_controller.rb
@@ -6,6 +6,7 @@ class CompletadosController < ApplicationController
     @completado = Completado.new(tarea: @tarea, usuario: current_usuario)
       if @completado.save
         redirect_to tareas_path, notice: 'La tarea ha sido completada'
+        
       else
         redirect_to tareas_path alert: 'La tarea no ha podido ser completada'
       end
